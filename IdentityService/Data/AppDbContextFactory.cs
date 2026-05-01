19eea3e8393d6daaa9a412f6a10644d13069eec0
akshirsagar643 <akshirsagar643@gmail.com>
2026-05-01 15:36:42 +0530

    completed Identity Service changes

---
+++ IdentityService/Data/AppDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                "Server=.;Database=WealthPulseIdentityDb;Trusted_Connection=True;TrustServerCertificate=True");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
